1c817aa5e0d462c4ae22e37f4b00e955465d0d8e
José Alencar de Souza Rocha <jose_alencar_1403@hotmail.com>
2015-06-02 17:32:05 -0300

    feat(Aula02) Adicionar aula 02 C#

---
+++ src/TurboRango/TurboRango.ImportadorXML/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurboRango.Dominio;
+
+namespace TurboRango.ImportadorXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exemplo
+            //Restaurante restaurante = new Restaurante();
+
+            //restaurante.Nome = string.Empty;
+
+            //Console.WriteLine(!string.IsNullOrEmpty(restaurante.Nome) ? "Tem valor" : "Não tem valor");
+            #endregion
+
+            #region Importação XML
+            const string nomeArquivo = "restaurantes.xml";
+
+            var restaurantesXML = new RestaurantesXML(nomeArquivo);
+
+            var nomes = restaurantesXML.ObterNomes();
+
+            #endregion
+        }
+    }
+}
---
+++ src/TurboRango/TurboRango.ImportadorXML/RestaurantesXML.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TurboRango.ImportadorXML
+{
+    public class RestaurantesXML
+    {
+        public string NomeArquivo { get; private set; }
+
+
+        /// <summary>
+        /// Constrói RestaurantesXML a partir do nome do arquivo
+        /// </summary>
+        /// <param name="nomeArquivo">Nome do Arquivo XML</param>
+        public RestaurantesXML(string nomeArquivo)
+        {
+            this.NomeArquivo = nomeArquivo;
+        }
+
+        public IList<string> ObterNomes()
+        {
+            #region modo hard
+            //var resultado = new List<string>();
+
+            //var nodos = XDocument.Load(NomeArquivo).Descendants("restaurante");
+
+            //foreach (var item in nodos)
+            //{
+            //    resultado.Add(item.Attribute("nome").Value);
+            //}
+
+            //return resultado;
+            #endregion
+
+            #region modo query
+            //return (
+            //    from n in XDocument.Load(NomeArquivo).Descendants("restaurante")
+            //    orderby n.Attribute("nome").Value
+            //    select n.Attribute("nome").Value
+            //    ).ToList();
+            #endregion
+
+            return XDocument.Load(NomeArquivo).Descendants("restaurante")
+                .Select(n => n.Attribute("nome").Value).OrderBy(n => n).ToList();
+        }
+
+        public double CapacidadeMedia()
+        {
+            return (from n in XDocument.Load(NomeArquivo).Descendants("restaurante")
+                        select Convert.ToInt32(n.Attribute("capacidade").Value))
+                        .Average();
+        }
+
+        public double CapacidadeMaxima()
+        {
+            return (from n in XDocument.Load(NomeArquivo).Descendants("restaurante")
+                    select Convert.ToInt32(n.Attribute("capacidade").Value))
+                        .Max();
+        }
+    }
+}
